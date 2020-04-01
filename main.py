dc46ceba2ad03e9e77d22ba6550b415393242ac2
sunlf <admin@xcloudlive.com>
2020-04-01 15:19:42 +0800

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import json
+app = Flask(__name__)
+
+
+@app.route('/redfish/v1/Systems/1')
+def system():
+    with open('system.json') as f:
+        temp = json.load(f)
+    return jsonify(temp)
+
+
+if __name__ == '__main__':
+    app.run()
