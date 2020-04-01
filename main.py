5bd5f6a6fe8abc0aa38fc2c3a073b342c00481db
sunlf <admin@xcloudlive.com>
2020-04-01 18:48:57 +0800

    change

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
 import json
-app = Flask(__name__)
+from flask_basicauth import BasicAuth
 
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'john'
+app.config['BASIC_AUTH_PASSWORD'] = 'matrix'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/redfish/v1/Systems/1')
+@basic_auth.required
 def system():
     with open('system.json') as f:
         temp = json.load(f)
