c77ba197671e237de4540a156ea60840501e6dea
xcloudlive <62527505+xcloudlive@users.noreply.github.com>
2020-04-03 14:41:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import json
 from flask_basicauth import BasicAuth
 
@@ -15,6 +15,12 @@ def system():
         temp = json.load(f)
     return jsonify(temp)
 
+@app.route('/sunlf', methods=['POST'])
+@basic_auth.required
+def sunlf():
+    print(request.headers)
+    print(request.json)
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
